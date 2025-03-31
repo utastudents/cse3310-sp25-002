aa3dab01cafca01728e4e60c48cbc69157e1dddf
Najat Hussein <nih6584@mavs.uta.edu>
2025-03-31 17:49:51 -0500

    labeling as method

--- checkers/src/main/java/uta/cse3310/GameTermination/BoardState.java
+++ checkers/src/main/java/uta/cse3310/GameTermination/BoardState.java
@@ -1,7 +1,7 @@
 package uta.cse3310.GameTermination;
 
 public class BoardState {
-	public boolean hasLegalMoves(int playerId) //checks if player has legal moves left 
+	public boolean hasLegalMoves(int playerId) //METHOD: checks if player has legal moves left 
 	{
 
 	}
