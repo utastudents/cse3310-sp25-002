546586560a8a7d225f9f43a537ab8c7e09a276a4
Najat Hussein <nih6584@mavs.uta.edu>
2025-03-31 17:44:35 -0500

    adding hasLegalMoves method

--- checkers/src/main/java/uta/cse3310/GameTermination/BoardState.java
+++ checkers/src/main/java/uta/cse3310/GameTermination/BoardState.java
@@ -1,5 +1,19 @@
 package uta.cse3310.GameTermination;
 
 public class BoardState {
+	public boolean hasLegalMoves(int playerId) //checks if player has legal moves left 
+	{
+
+	}
+
+
+
+
+
+
+
+
+
+
     
 }
