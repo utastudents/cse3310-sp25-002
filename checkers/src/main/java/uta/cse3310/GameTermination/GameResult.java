f1b6de39f774c4e3df57e8fd24d9d18ebc2b16ba
“Prof <aashishkafle001@gmail.com>
2025-04-02 16:44:18 -0500

    added a constructor in gameresult

--- checkers/src/main/java/uta/cse3310/GameTermination/GameResult.java
+++ checkers/src/main/java/uta/cse3310/GameTermination/GameResult.java
@@ -7,6 +7,12 @@ public class GameResult {
     //stores scores for each player
     Map<String, Integer> playerScores;
 
+    //constructor to initialize scores
+    public GameResult( Map<String, Integer> playerScores)
+    {
+        this.playerScores = playerScores;
+    }
+
     //will display scores after game ends for both players.
     public void displayLeaderboard(Player player1, Player player2){
 
