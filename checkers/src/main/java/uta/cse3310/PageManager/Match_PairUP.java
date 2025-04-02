f946da25f576d67e548da19d2f82d0cb2e59045e
Pragyan Bhattarai <147287372+Pragyan455@users.noreply.github.com>
2025-04-02 01:07:05 -0500

    Classes for external Data Structures
    Based Upon user interface of PairUp the necessary fields for data structures is created

---
+++ checkers/src/main/java/uta/cse3310/PageManager/Match_PairUP.java
@@ -0,0 +1,13 @@
+package uta.cse3310.PageManager;
+
+import java.time.LocalDateTime;
+
+public class Match_PairUP {
+    public String gameId;
+    public String player1Id;
+    public String player2Id;
+    public String player1Name;
+    public String player2Name;
+    public boolean isBotGame;
+    public LocalDateTime timestamp;
+}
---
+++ checkers/src/main/java/uta/cse3310/PageManager/PlayerEntry_PairUP.java
@@ -0,0 +1,10 @@
+package uta.cse3310.PageManager;
+
+import java.time.LocalDateTime;
+
+public class PlayerEntry_PairUP {
+    public String playerId;
+    public String playerName;
+    public boolean playAgainstBot;
+    public LocalDateTime timestamp;
+}
