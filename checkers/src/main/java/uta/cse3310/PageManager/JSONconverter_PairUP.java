2be8cb62784db352f31d21b87603da2c5eca524a
Pragyan Bhattarai <147287372+Pragyan455@users.noreply.github.com>
2025-04-02 02:29:44 -0500

    Class to convert to and from JSON
    parses JSON file from UI and transfers to PairUP
    converts data from PairUP to JSON and passes it to UI

---
+++ checkers/src/main/java/uta/cse3310/PageManager/JSONconverter_PairUP.java
@@ -0,0 +1,17 @@
+uta.cse3310.PageManager;
+
+import java.util.List;
+
+public class JSONconverter_PairUP{ 
+    public static List<PlayerEntry_PairUP> parsePlayersFromJson(String json) 
+    {
+
+        return List.of(); 
+    }
+
+    public static String convertMatchesToJson(List<Match> matches) 
+    {
+        
+        return "{}"; 
+    }
+}
