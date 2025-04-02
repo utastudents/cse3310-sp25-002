d3018af88df61dbae8fe03584e042a59919eeb5d
suhas-arg <123773070+suhas-arg@users.noreply.github.com>
2025-04-01 20:07:30 -0500

    Add files via upload

---
+++ checkers/src/main/java/uta/cse3310/GameManager/Player.java
@@ -0,0 +1,17 @@
+public class Player{
+     private int playerId;
+     private int score;
+     private int pieceCount;
+
+     public Player(int playerId){
+          this.playerId = playerId;
+          this.score = 0;
+          this.pieceCount = 12;
+     }
+
+     public int getPlayerId(){return playerId;}
+     public int getScore(){return score;}
+     public void addScore(int points){score += points;}
+     public int getPieces(){return pieceCount;}
+     public void takePieces(int numPieces){pieceCount-= numPieces;}
+}
