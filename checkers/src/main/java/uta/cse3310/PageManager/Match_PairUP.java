0f8282c6e0fc8f8ee18f7cb0bc223cf9e6d9f9a8
Pragyan Bhattarai <147287372+Pragyan455@users.noreply.github.com>
2025-04-02 02:46:07 -0500

    Updated set of fields required for Match_PairUP

--- checkers/src/main/java/uta/cse3310/PageManager/Match_PairUP.java
+++ checkers/src/main/java/uta/cse3310/PageManager/Match_PairUP.java
@@ -3,11 +3,6 @@ package uta.cse3310.PageManager;
 import java.time.LocalDateTime;
 
 public class Match_PairUP {
-    public String gameId;
-    public String player1Id;
-    public String player2Id;
-    public String player1Name;
-    public String player2Name;
-    public boolean isBotGame;
+     public String ActivePlayers[];
     public LocalDateTime timestamp;
 }
