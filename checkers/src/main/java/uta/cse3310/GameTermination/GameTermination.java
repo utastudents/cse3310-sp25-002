c1e8557dbe26a9e94eeddd5ebd78154ec4b98481
uta-pxa9412 <pxa9412@mavs.uta.edu>
2025-04-02 10:40:10 -0500

    Group 35: Added method signatures for GameTermiation and GameResult

--- checkers/src/main/java/uta/cse3310/GameTermination/GameTermination.java
+++ checkers/src/main/java/uta/cse3310/GameTermination/GameTermination.java
@@ -1,5 +1,13 @@
 package uta.cse3310.GameTermination;
 
 public class GameTermination {
+    //will decide if the game has winner
+    public boolean hasAWinner(BoardState boardState){
+        //this method will return either true or false depending if there is a winner or not.
+        //this will depend on having no legal moves or capturing all of the opponet pieces.
+    }
 
+    public boolean isGameDraw(BoardState boardState){
+        //this method will return if game has no winner and is draw
+    }
 }
