e8a3379324c3f33026af66682c2d2a95c23832a2
tonypham998 <tonypham887@gmail.com>
2025-04-03 03:13:11 +0000

    user informtaion and method for

--- checkers/src/main/java/uta/cse3310/PageManager/UserEvent.java
+++ checkers/src/main/java/uta/cse3310/PageManager/UserEvent.java
@@ -2,6 +2,23 @@ package uta.cse3310.PageManager;
 
 public class UserEvent {
     public Integer id; // the user id that created the message
-    String msg;
+
+    // user information
+    public String UserName;  //userName of player
+    public String Password;  //password of player
+
+    // game status
+    public int win;                   // number of wins
+    public int lose;                 // number of losses
+    public int TotalGames;           //total games played
+    public String rank;              // dont know the ranking system yet
+
+    String msg;     
+
+
+    public static UserEvent fromJSON(Sting jsonStr){
+        // TODO: implement this method
+        // This method should parse the JSON string and create a UserEvent object
+    }
 
 }
