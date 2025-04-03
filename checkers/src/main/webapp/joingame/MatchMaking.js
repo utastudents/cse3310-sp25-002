9e04d0cd607f116a46ea0c1bcdac90c2e3128d36
Victoria Phan <vvp3735@gmail.com>
2025-04-03 18:25:30 -0500

    Updating match making functions

--- checkers/src/main/webapp/joingame/MatchMaking.js
+++ checkers/src/main/webapp/joingame/MatchMaking.js
@@ -1,47 +1,38 @@
+
 class Matchmaker {
     constructor() {
-        this.queue = []; // Queue for players, do I need waitlist class?
+        this.queue = []; // Queue for players waiting for a match
     }
 
+    addToQueue(playerID) {
+        // TODO: Send event to Page Manager that player has joined queue
 
-    addToQueue(player) {
-        this.queue.push(player);
-        console.log('${player} added to queue.');
-        this.tryMatchPlayers();
+        // Adds a player to the matchmaking queue.
+        // TODO: Implement logic to send event to Page Manager when a player joins the queue.
+        // Use {string} for playerID - unique ID of the player joining the queue.
     }
 
+    requestPlayerMatch(playerID) {
+        // TODO: Send event to Page Manager requesting a player match
 
-    tryMatchPlayers() {
-        if (this.queue.length >= 2) {
-            // match 2 human players
-            let player1 = this.queue.shift();
-            let player2 = this.queue.shift();
-            console.log('Match created: ${player1} vs ${player2}');
-
-
-            // Send to communication
-            this.sendMatchInfo(player1, player2, "Player");
-        }
+        // Handles when a player clicks the "Challenge Player" button.
+        // TODO: Notify Page Manager that the player wants a human opponent.
+        // Use {string} playerID - unique ID of the player that wants to challenge.
     }
 
+    requestBotMatch(playerID) {
+        // TODO: Send event to Page Manager requesting a bot match
 
-    requestBotMatch(player) {
-        console.log('${player} wants to play against a bot.');
-
-        // Send to communication
-        this.sendMatchInfo(player, "Bot", "Bot");
+        // Handles when a player clicks the "Challenge Bot" button.
+        // TODO: Notify Page Manager that the player wants to play against a bot.
+        // Use {string} playerID - unique ID of the player that wants a bot match.
     }
 
+    removeFromQueue(playerID) {
+        // TODO: Remove player from queue and notify Page Manager
 
-    matchWithBot(player){
+        // TODO: Implement logic to remove player and notify Page Manager.
+        // Use {string} playerID - unique ID of the player to be removed.
 
-
-        console.log('Match created: ${player} vs ${bot}');
-
-
-        // Send to communication
-        this.sendMatchInfo(player, bot, "Bot");
     }
-
-
 }
