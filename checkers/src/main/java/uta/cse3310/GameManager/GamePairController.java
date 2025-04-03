948187e51a45b92708f241cd6b7012f1e969fed7
uta-sxl8084 <sxl8084@mavs.uta.edu>
2025-04-03 16:55:34 -0500

    Update GamePairController.java

--- checkers/src/main/java/uta/cse3310/GameManager/GamePairController.java
+++ checkers/src/main/java/uta/cse3310/GameManager/GamePairController.java
@@ -1,5 +1,33 @@
 package uta.cse3310.GameManager;
 
-public class GamePairController {
-     
+import uta.cse3310.PairUp.PairUp;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+
+public class GamePairController{
+    private PairUp pairUpSys;
+    private ObjectMapper objectMapper; //JSON conversion
+
+    public GamePairController(PairUp pairUpSys){
+        this.pairUpSys = pairUpSys;
+        this.objectMapper = new ObjectMapper();
+    }
+
+    public Game initializeGame(String pairUpJson){
+        try{
+            PairUp.Match match = objectMapper.readValue(pairUpJson, PairUp.Match.class); //Parse JSON input to extract info from pairUp
+
+            int player1ID = Integer.parseInt(match.getPlayer1Id());
+            int player2ID = Integer.parseInt(match.getPlayer2Id());
+
+            String gameID = match.getGameId();
+            Game newGame = new Game(player1ID, player2ID, gameID);
+
+            return newGame;
+        }
+        catch(IOException e){
+            System.err.println("Error initializing game from JSON: " + e.getMessage());
+            return null;
+        }
+    }
 }
