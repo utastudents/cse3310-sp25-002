fd513560d3558f18d28141b068c7bc0b4d47a4d5
PrabinLamichhane1 <pxl0919@mavs.uta.edu>
2025-04-03 13:59:20 -0500

    minor changes

--- checkers/src/main/java/uta/cse3310/DB/Validate.java
+++ checkers/src/main/java/uta/cse3310/DB/Validate.java
@@ -9,7 +9,7 @@ public class Validate
     {
         // This method will be used to validate the user from the database.
         // It will take the player's ID as input and return their rank.
-
+        // It will check if the user exists in the database and return true or false.
     }
 
 
