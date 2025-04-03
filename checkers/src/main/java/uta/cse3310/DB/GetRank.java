10e90e264b3d3c2e7189b89e3a526f4d68b9eb1e
PrabinLamichhane1 <pxl0919@mavs.uta.edu>
2025-04-03 13:49:26 -0500

    "added delete player and other functions"

--- checkers/src/main/java/uta/cse3310/DB/GetRank.java
+++ checkers/src/main/java/uta/cse3310/DB/GetRank.java
@@ -6,7 +6,10 @@ import java.sql.SQLException;
 public class GetRank
 {
     
-
-
+    public static void getRank()
+    {
+        // This method will be used to get the rank of a player from the database.
+        // It will take the player's username as input and return their rank.
+    }
 
 }
--- checkers/src/main/java/uta/cse3310/DB/Validate.java
+++ checkers/src/main/java/uta/cse3310/DB/Validate.java
@@ -7,7 +7,8 @@ public class Validate
 {
     public static void ValidateUser()
     {
-
+        // This method will be used to validate the user from the database.
+        // It will take the player's ID as input and return their rank.
 
     }
 
