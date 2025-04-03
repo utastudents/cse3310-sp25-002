2844cc1d463e69053af5df55c8900239c7e789d7
Jakub Wojtowicz <jmw4895@mavs.uta.edu>
2025-04-03 14:06:18 -0700

    Add files via upload

---
+++ checkers/src/main/java/uta/cse3310/GamePlay/rules.java
@@ -0,0 +1,24 @@
+package uta.cse3310.GamePlay;
+
+public class rules
+{
+    protected boolean inBounds()
+    {
+        return false; //Default
+    }
+
+    protected boolean isDiagonal()
+    {
+        return false; //Default
+    }
+
+    protected boolean makeKing()
+    {
+        return false; //Default
+    }
+
+    protected boolean isKing()
+    {
+        return false; //Default
+    }
+}
