c7fb58679c475e8bff0001780119a72d5366a62b
Andrew Truong <aht8637@mavs.uta.edu>
2025-04-03 17:02:37 -0500

    added occupied and pieceToMoves and added comments and parameters

--- checkers/src/main/java/uta/cse3310/GamePlay/rules.java
+++ checkers/src/main/java/uta/cse3310/GamePlay/rules.java
@@ -1,23 +1,38 @@
 package uta.cse3310.GamePlay;
 
+import java.util.LinkedList;
+
+import uta.cse3310.GameManager.Board;
+
+
+//class rules checks if the move is legal
 public class rules
 {
-    protected boolean inBounds()
+    //checks if the move is within the bounds of the board
+    //8x8 board
+    //issue (maybe): class Move needs to be visible to get move set
+    static protected boolean inBounds(LinkedList moves)
+    {
+        int rowSize = 8, colSize = 8;
+        return false; //Default
+    }
+
+    //checks if the piece moves diagonally up-right and up-left
+    //for king: moves diagonally down-right and down-left in addition to above
+    static protected boolean isDiagonal(boolean isKing, LinkedList moves, Board board)
     {
         return false; //Default
     }
 
-    protected boolean isDiagonal()
+    //checks if the square being moved to is occupied by a piece
+    static protected boolean occupied(LinkedList moves, Board board)
     {
         return false; //Default
     }
 
-    protected boolean makeKing()
-    {
-        return false; //Default
-    }
-
-    protected boolean isKing()
+    //checks how many spots moved up to compared to number of pieces
+    //0 pieces = 1, 1 piece = 2, 2 pieces = 4 etc...
+    static protected boolean pieceToMoves(LinkedList moves, Board board)
     {
         return false; //Default
     }
