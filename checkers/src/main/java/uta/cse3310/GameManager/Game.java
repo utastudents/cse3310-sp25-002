2a368946f837dedeac8ec2479dae970ef815cf74
uta-jel3351 <jel3351@mavs.uta.edu>
2025-04-02 22:16:29 -0500

    a couple methods added

--- checkers/src/main/java/uta/cse3310/GameManager/Game.java
+++ checkers/src/main/java/uta/cse3310/GameManager/Game.java
@@ -6,12 +6,35 @@ public class Game
     private Board board;
     private Player player1;
     private Player player2;
+    private static int gameNumber;
 
-    public Game(int player1id, int player2id, boolean player1color, boolean player2color)
+
+    public Game(int player1id, int player2id, boolean player1color, boolean player2color, int gameNumber)
     {
         board = new Board();
         player1 = new Player(player1id, player1color);
         player2 = new Player(player2id, player2color);
+        this.gameNumber = gameNumber;
+    }
+    public int getPlayer1ID()
+    {
+        return player1.getPlayerId();
+    }
+    public int getPlayer2ID()
+    {
+        return player2.getPlayerId();
+    }
+    public boolean getPlayer1Color()
+    {
+        return player1.getColor();
+    }
+    public boolean getPlayer2Color()
+    {
+        return player2.getColor();
+    }
+    public Board getBoard()
+    {
+        return board;
     }
     
 }
