9ae1e24846d980a0c2eec0b8a616478b0db3cd00
uta-axs4549 <axs4549+1@mavs.uta.edu>
2025-04-03 11:26:57 -0400

    Added Classes for Validation and Rank

---
+++ checkers/src/main/java/uta/cse3310/DB/GetRank.java
@@ -0,0 +1,12 @@
+package uta.cse3310.DB;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class GetRank
+{
+    
+
+
+
+}
---
+++ checkers/src/main/java/uta/cse3310/DB/Validate.java
@@ -0,0 +1,15 @@
+package uta.cse3310.DB;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Validate
+{
+    public static void User()
+    {
+
+        
+    }
+
+
+}
