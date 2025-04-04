12501684024f9e983dd63a8789d50ac1e5391bbf
uta-sxl8084 <sxl8084@mavs.uta.edu>
2025-04-04 11:12:13 -0500

    Update GamePairController.java

--- checkers/src/main/java/uta/cse3310/GameManager/GamePairController.java
+++ checkers/src/main/java/uta/cse3310/GameManager/GamePairController.java
@@ -5,15 +5,13 @@ import uta.cse3310.PairUp.Match;
 import java.io.IOException;
 
 public class GamePairController{
-    // bd9659 private PairUp pairUpSys;
-
+    /*private PairUp pairUpSys;
     //private ObjectMapper objectMapper; (JSON conversion)
 
-    //bd96959 public GamePairController(PairUp pairUpSys){
+    public GamePairController(PairUp pairUpSys){
         /*this.pairUpSys = pairUpSys;
-    // bd9659  this.objectMapper = new ObjectMapper();*/
-
-    // bd9659 }
+        this.objectMapper = new ObjectMapper();
+    }*/
 
     public Game initializeGame(String pairUpJson){
         /*try{
@@ -22,7 +20,7 @@ public class GamePairController{
             int player1ID = Integer.parseInt(match.getPlayer1Id());
             int player2ID = Integer.parseInt(match.getPlayer2Id());
 
-	    boolean player1color = true;
+	        boolean player1color = true;
             boolean player2color = false;
 
             int gameNumber;
@@ -36,10 +34,9 @@ public class GamePairController{
             Game newGame = new Game(player1ID, player2ID, player1color, player2color, gameNumber);
 
             return newGame;
-        // bd9659 } catch (IOException e) {
-        // bd9659    System.err.println("Error initializing game from JSON: " + e.getMessage());*/
-        // bd9650    return null;
-        // bd9659 }
-        return null;
+        } catch (IOException e) {
+            System.err.println("Error initializing game from JSON: " + e.getMessage());*/
+            return null;
+        }
     }
-}
+    
