b82036a77e13c86d0325ea329ee2b6dcacf73168
Brooke Sayer <bxs0056@mavs.uta.edu>
2025-04-03 23:11:58 -0500

    Waitlist Class Draft

---
+++ checkers/src/main/webapp/joingame/Waitlist.js
@@ -0,0 +1,24 @@
+class Waitlist{
+	constructor(id, username, place){
+	this.id = id;
+	this.username = username;
+	}
+
+	const player = [id, username];
+
+	//Change above to whatever data gotten from Data.js or Communication.js
+	//
+	
+	add(player){	//Add a player's username, id onto the waitlist
+		
+		return player;
+	}
+	
+	remove(player){	//Remove a player's info from the waitlist when leaving the game
+		
+	}
+	
+	updateWaitlist(player){	//Update waitlist and placement's of players each time another player is added/removed
+
+	}
+}
