4710a63923afe345ac5ad37e63ee85bf4771f857
Burhanuddin Aliasgar Khokhar <bxk9892@mavs.uta.edu>
2025-04-04 16:22:34 -0500

    update PlayerInMatchmaking.java

--- checkers/src/main/java/uta/cse3310/PairUp/PlayerInMatchmaking.java
+++ checkers/src/main/java/uta/cse3310/PairUp/PlayerInMatchmaking.java
@@ -10,8 +10,43 @@ package uta.cse3310.PairUp;
     up with an appropriate opponent.
 */
 public class PlayerInMatchmaking {
-    public PlayerInMatchmaking() {
 
+    private long timeOfEntry;
+    private String playerID;
+    private String playerName;
+    private boolean playAgainstBot;
+    private int wins;
+
+    //  constructor
+    public PlayerInMatchmaking(long timeOfEntry, String playerID, String playerName, boolean playAgainstBot, int wins) {
+        this.timeOfEntry = timeOfEntry;
+        this.playerID = playerID;
+        this.playerName = playerName;
+        this.playAgainstBot = playAgainstBot;
+        this.wins = wins;
     }
+    
+    
+    //  getters
+    public long getTimeOfEntry() {
+        return timeOfEntry;
+    }
+    
+    public String getPlayerID() {
+        return playerID;
+    }
+    
+    public String getPlayerName() {
+        return playerName;
+    }
+    
+    public boolean isPlayAgainstBot() {
+        return playAgainstBot;
+    }
+    
+    public int getWins() {
+        return wins;
+    }
+    
 
 }
