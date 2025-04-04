8d1e83a2621bb19432d809f9fad7d7837a69cda5
cxt4513 <cxt4513@mavs.uta.edu>
2025-04-03 23:51:58 -0500

    Fixed grammar in comment

--- checkers/src/main/java/uta/cse3310/PairUp/Matchmaking.java
+++ checkers/src/main/java/uta/cse3310/PairUp/Matchmaking.java
@@ -3,8 +3,9 @@ package uta.cse3310.PairUp;
 /*
     The Matchmaking class will deal with all things
     matchmaking. It holds all the players that are
-    looking for a match and deals with pairing them
-    up, and removing them from matchmaking.
+    looking for a match. It deals with pairing them
+    up, and removes them from matchmaking once they
+    find a match, or if they quit matchmaking.
 */
 public class Matchmaking {
     public Matchmaking() {
