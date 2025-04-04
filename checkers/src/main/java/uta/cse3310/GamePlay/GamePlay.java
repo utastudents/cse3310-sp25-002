9837828a6550e9f6c9d51890af50df1fecb4f365
Jakub Wojtowicz <jmw4895@mavs.uta.edu>
2025-04-04 10:29:59 -0500

    Add files via upload

--- checkers/src/main/java/uta/cse3310/GamePlay/GamePlay.java
+++ checkers/src/main/java/uta/cse3310/GamePlay/GamePlay.java
@@ -2,13 +2,5 @@ package uta.cse3310.GamePlay;
 
 public class GamePlay
 {
-    public static void main(String[] args)
-    {
-        //Stuff to check logic
-        
-        
-
-    }
-
-
+    
 }
