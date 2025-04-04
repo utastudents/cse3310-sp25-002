4b240fcecef874d094c5e9e132f1f996baa86e7a
uta-sxl8084 <sxl8084@mavs.uta.edu>
2025-04-04 11:13:25 -0500

    Update GameManager.java

--- checkers/src/main/java/uta/cse3310/GameManager/GameManager.java
+++ checkers/src/main/java/uta/cse3310/GameManager/GameManager.java
@@ -4,8 +4,8 @@ import uta.cse3310.GamePlay.GamePlay;
 import uta.cse3310.GameTermination.GameTermination;
 import uta.cse3310.Bot.BotI.BotI;
 import uta.cse3310.Bot.BotII.BotII;
-//import uta.cse3310.Controllers.GamePageController;  // bd9659
-//import uta.cse3310.Controllers.GamePairController;  // bd9659
+//import uta.cse3310.Controllers.GamePageController;  
+//import uta.cse3310.Controllers.GamePairController;  
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,8 +16,8 @@ public class GameManager {
     BotI b1;
     BotII b2;
     List<Game> games;
-    // bd9659 GamePageController gamePageController;
-    // bd9659 GamePairController gamePairController;
+    // GamePageController gamePageController;
+    // GamePairController gamePairController;
 
     public GameManager() {
         gp = new GamePlay();
@@ -25,13 +25,13 @@ public class GameManager {
         b1 = new BotI();
         b2 = new BotII();
         games = new ArrayList<>();
-        // bd9659  gamePageController = new GamePageController();
-        // bd9659  gamePairController = new GamePairController();
+        //  gamePageController = new GamePageController();
+        //  gamePairController = new GamePairController();
     }
 
     public void addGame(Game game) {
         games.add(game);
-        // bd9659 gamePairController.registerGame(game);
+        // gamePairController.registerGame(game);
     }
 
     public Game getGame(int gameNumber) {
@@ -48,10 +48,10 @@ public class GameManager {
     }
 
     public void sendGameToComponents(Game game) {
-        // bd9659 gp.processGame(game);
-        // bd9659 gt.evaluateGame(game);
-        // bd9659 b1.analyzeGame(game);
-        // bd9659 b2.analyzeGame(game);
-        // bd9659 gamePageController.updateGame(game);
+        // gp.processGame(game);
+        // gt.evaluateGame(game);
+        // b1.analyzeGame(game);
+        // b2.analyzeGame(game);
+        // gamePageController.updateGame(game);
     }
 }
