7cd6608b20324e3f13a79c7294281978f29f11fe
uta-sxl8084 <sxl8084@mavs.uta.edu>
2025-04-04 10:41:52 -0500

    Only defining methods

--- checkers/src/main/java/uta/cse3310/GameManager/GamePairController.java
+++ checkers/src/main/java/uta/cse3310/GameManager/GamePairController.java
@@ -1,32 +1,41 @@
 package uta.cse3310.GameManager;
 
-import uta.cse3310.PairUp.PairUp;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import uta.cse3310.PairUp.Match;
+//import com.fasterxml.jackson.databind.ObjectMapper; (library for JSON conversion)
 import java.io.IOException;
 
 public class GamePairController{
     private PairUp pairUpSys;
-    private ObjectMapper objectMapper; //JSON conversion
+    //private ObjectMapper objectMapper; (JSON conversion)
 
     public GamePairController(PairUp pairUpSys){
-        this.pairUpSys = pairUpSys;
-        this.objectMapper = new ObjectMapper();
+        /*this.pairUpSys = pairUpSys;
+        this.objectMapper = new ObjectMapper();*/
     }
 
     public Game initializeGame(String pairUpJson){
-        try{
+        /*try{
             PairUp.Match match = objectMapper.readValue(pairUpJson, PairUp.Match.class); //Parse JSON input to extract info from pairUp
 
             int player1ID = Integer.parseInt(match.getPlayer1Id());
             int player2ID = Integer.parseInt(match.getPlayer2Id());
 
-            String gameID = match.getGameId();
-            Game newGame = new Game(player1ID, player2ID, gameID);
+	        boolean player1color = true;
+            boolean player2color = false;
+
+            int gameNumber;
+            try {
+                gameNumber = Integer.parseInt(match.getGameId());
+            } catch(NumberFormatException e){
+                System.err.println("Invalid game ID format: " + match.getGameId());
+                return null;
+            }
+
+            Game newGame = new Game(player1ID, player2ID, player1color, player2color, gameNumber);
 
             return newGame;
-        }
-        catch(IOException e){
-            System.err.println("Error initializing game from JSON: " + e.getMessage());
+        } catch (IOException e) {
+            System.err.println("Error initializing game from JSON: " + e.getMessage());*/
             return null;
         }
     }
