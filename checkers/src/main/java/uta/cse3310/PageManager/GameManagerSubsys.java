0b00f94dcaa6180d01b21dd6c6b6537e7977565c
root <root@priscilla>
2025-04-03 21:27:09 -0500

    Added GameManagerSubsys for page manager

---
+++ checkers/src/main/java/uta/cse3310/PageManager/GameManagerSubsys.java
@@ -0,0 +1,22 @@
+package uta.cse3310.PageManager;
+import uta.cse3310.GameManager.GameManager;
+
+public class GameManagerSubsys {
+
+    private final GameManager gameManager;
+
+    public GameManagerSubsys(GameManager gameManager) {
+        this.gameManager = gameManager;
+     }
+
+
+     public String processMove(String jsonObj) {
+        // TODO: implement
+        return null;
+    }
+    
+    public String getGameState(String gameID) {
+        // TODO: implement
+        return null;
+    }
+}
