9bf1a762e6b5f89e9361daf11a04637d50f70d64
utaixa5131 <ixa5131@mavs.uta.edu>
2025-04-05 02:19:29 -0500

    Change parameters needed for Pairup

--- checkers/src/main/java/uta/cse3310/PairUp/PairUp.java
+++ checkers/src/main/java/uta/cse3310/PairUp/PairUp.java
@@ -1,15 +1,46 @@
+// -- Import Dependencies --
 package uta.cse3310.PairUp;
+import java.util.LinkedHashMap;
 
-import uta.cse3310.DB.DB;
+
 
 /*
     The PairUp class will handle recieving info from the page manager system
     and relaying it to other parts of the system, this includes calls of adding players to our
     queue as well as requesting to remove a player from the queue in the case they log off
 */
+
+ 
+
 public class PairUp {
-    public PairUp(DB db) {
+    //
+    private LinkedHashMap<String, PlayerInMatchmaking> MatchMap;
+
+  
+    public PairUp() 
+    {
+        this.MatchMap = new LinkedHashMap<>();
+    }
+
+
+    // Add a player to the Map
+    public void AddPlayer(long timeOfEntry, String playerID, String playerName, boolean playAgainstBot, int wins)
+    {
+        // Create a new instance of a player from the player in match making class
+        PlayerInMatchmaking player = new PlayerInMatchmaking(timeOfEntry, playerID, playerName, playAgainstBot, wins);
+        
+        // Add said player to LinkedHashMap
+        MatchMap.put(playerID, player); 
 
     }
+
+    public void removePlayer(String PlayerID)
+    {
+
+        MatchMap.remove(PlayerID);
+
+    }
+
+    
 
 }
