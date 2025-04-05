075e5e60f3d872f9bdd2fcf648f479d9423e08ed
Victoria Phan <vvp3735@gmail.com>
2025-04-04 22:42:58 -0500

    Clarifying where user events are being sent

--- checkers/src/main/webapp/joingame/MatchMaking.js
+++ checkers/src/main/webapp/joingame/MatchMaking.js
@@ -5,33 +5,43 @@ class Matchmaker {
     }
 
     addToQueue(playerID) {
-        // TODO: Send event to Page Manager that player has joined queue
+        // TODO: Send event to Join Game - Communication.js that player has joined queue
 
         // Adds a player to the matchmaking queue.
-        // TODO: Implement logic to send event to Page Manager when a player joins the queue.
+        // TODO: Implement logic to send event to Join Game - Communication.js when a player joins the queue.
         // Use {string} for playerID - unique ID of the player joining the queue.
     }
 
     requestPlayerMatch(playerID) {
-        // TODO: Send event to Page Manager requesting a player match
+        // TODO: Send event to Join Game - Communication.js requesting a player match
 
         // Handles when a player clicks the "Challenge Player" button.
-        // TODO: Notify Page Manager that the player wants a human opponent.
+        // TODO: Notify Join Game - Communication.js that the player wants a human opponent.
         // Use {string} playerID - unique ID of the player that wants to challenge.
+        // Should be a button
     }
 
     requestBotMatch(playerID) {
-        // TODO: Send event to Page Manager requesting a bot match
+        // TODO: Send event to Join Game - Communication.js requesting a bot match
 
         // Handles when a player clicks the "Challenge Bot" button.
-        // TODO: Notify Page Manager that the player wants to play against a bot.
+        // TODO: Notify Join Game - Communication.js that the player wants to play against a bot.
         // Use {string} playerID - unique ID of the player that wants a bot match.
+        // Should be a button
+    }
+
+
+    requestSpectateBotVsBot(playerID) {
+        // TODO: Send signal to Join Game - Communication.js that this player wants to watch two bots play.
+        // Called when a player wants to spectate a match between two bots.
+        // Use {string} playerID - ID of the player requesting to watch bots.
+        // Should be a button
     }
 
     removeFromQueue(playerID) {
-        // TODO: Remove player from queue and notify Page Manager
+        // TODO: Remove player from queue and notify Join Game - Communication.js
 
-        // TODO: Implement logic to remove player and notify Page Manager.
+        // TODO: Implement logic to remove player and notify Join Game - Communication.js.
         // Use {string} playerID - unique ID of the player to be removed.
 
     }
