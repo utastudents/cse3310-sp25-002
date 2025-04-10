8fce6a4c8ecf263788525839692c788a82967821
Pratik-ax <pxg7756@mavs.uta.edu>
2025-04-10 14:46:06 -0500

    Update JoinGameHandler.java

--- checkers/src/main/java/uta/cse3310/PageManager/JoinGameHandler.java
+++ checkers/src/main/java/uta/cse3310/PageManager/JoinGameHandler.java
@@ -9,23 +9,23 @@ public class JoinGameHandler {
 
     public void processJoinGame(Map<String, String> joinData) {
         String username = joinData.get("username");
-        String userID = joinData.get("userID");
+        int Client ID = joinData.get("Client ID");
         String gameMode = joinData.get("gameMode");
 
         if ("Multiplayer".equalsIgnoreCase(gameMode)) {
-            sendAvailabilityToDB(userID, username);
+            sendAvailabilityToDB(Client ID, username);
         } else if ("Bot".equalsIgnoreCase(gameMode)) {
-            sendToPairUpForBot(username, userID);
+            sendToPairUpForBot(username, Client ID);
         }
     }
 
-    private void sendAvailabilityToDB(String userID, String username) {
+    private void sendAvailabilityToDB(int Client ID, String username) {
         System.out.println("Flagging user as available in DB...");
-        System.out.println("Username: " + username + ", UserID: " + userID);
+        System.out.println("Username: " + username + ", Client ID: " + Client ID);
     }
 
-    private void sendToPairUpForBot(String username, String userID) {
+    private void sendToPairUpForBot(String username, int Client ID) {
         System.out.println("Sending request to PairUp for bot match...");
-        System.out.println("Username: " + username + ", UserID: " + userID);
+        System.out.println("Username: " + username + ", Client ID: " + Client ID);
     }
 }
