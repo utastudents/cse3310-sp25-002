dbc25d67362fc543f7ecb7b2dc8b43255ca0a16e
utaixa5131 <ixa5131@mavs.uta.edu>
2025-04-14 17:10:39 -0500

    Create template for each Pairup Test

---
+++ checkers/src/test/java/cse3310/uta/PairUpTests/Test1.java
@@ -0,0 +1,5 @@
+package cse3310.uta.PairUpTests;
+
+import org.junit.Test;
+import uta.cse3310.PairUp.PairUp;
+import static org.junit.Assert.*;
---
+++ checkers/src/test/java/cse3310/uta/PairUpTests/Test3.java
@@ -0,0 +1,14 @@
+package cse3310.uta.PairUpTests;
+
+import org.junit.Test;
+import uta.cse3310.PairUp.Matchmaking;
+import static org.junit.Assert.*;
+
+public class Test3 
+{
+    @Test
+    public void TestAddPlayer()
+    {
+        
+    }
+}
