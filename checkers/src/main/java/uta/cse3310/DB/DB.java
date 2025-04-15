7328add3503f817ea5f84436260caa41418f9780
Kevin <kevin.alvarez2@mavs.uta.edu>
2025-04-15 13:25:28 -0500

    added the create table statement

--- checkers/src/main/java/uta/cse3310/DB/DB.java
+++ checkers/src/main/java/uta/cse3310/DB/DB.java
@@ -1,24 +1,43 @@
 package uta.cse3310.DB;
 
-import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DB
 {
 	public static void createTable()
 	{
-		/*string url;
-
-		string CreateStatement = "CREATE TABLE IF NOT EXISTS users (\n"
+		String createStatement = "CREATE TABLE IF NOT EXISTS USERS (\n"
                 + " id INTEGER PRIMARY KEY AUTOINCREMENT,\n"
-                + " username TEXT NOT NULL,\n"
-                + ")"; //here the schema is to be established and create statement 
+                + " username TEXT NOT NULL UNIQUE,\n"
+                + "rank INTEGER NOT NULL DEFAULT 0)"; //here the schema is to be established and create statement 
 		
-		string stmt; */// will be used to during implementation of CREATE statement for table   
+		try(Connection connection = SQLiteConnector.connect();
+			Statement stmt = connection.createStatement()) 
+		{
+			if(connection != null)
+			{
+				stmt.execute(createStatement);
+			}
+			else
+			{
+				System.err.println("Failed to connect to database in Create table command");
+			}
+		}
+		catch(SQLException e)
+		{
+			System.err.println("Error creating table: " + e.getMessage());
+		}
 	}
 
 	public static void insertUser()
 	{
+		//String insertStatement = "INSERT INTO users (username, rank) VALUES";
 		/*string url;
 
 		string Attributes; // will parse attributes from other interfaces
@@ -32,8 +51,11 @@ public class DB
 
 	public static void getLeaderboard()
 	{
-		/* string url;
-		string selectStatement; */ //will be used to create the select statements 
+		//List<String> leaderboard = new ArrayList<>();
+		//String selectStatement = "SELECT username, rank FROM USERS ORDER BY rank DESC";
+
+		//try (Connection connection = SQLiteConnector.connect();
+			 //leaderboardQuery )
 	}
 
 	public static void UpdatePlayer()
