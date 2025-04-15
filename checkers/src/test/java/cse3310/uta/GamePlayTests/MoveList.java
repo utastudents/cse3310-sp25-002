9ba10336dc2bca0d78f1361af72f289d97740ca0
Andrew Truong <aht8637@mavs.uta.edu>
2025-04-15 12:40:47 -0500

    created GamePlayTests folder and added MoveList test file

---
+++ checkers/src/test/java/cse3310/uta/GamePlayTests/MoveList.java
@@ -0,0 +1,14 @@
+package cse3310.uta.GamePlayTests;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class MoveList {
+
+    @Test
+    public void shouldBeTrue()
+    {
+        assertTrue(true);
+    }
+
+}
