5962f3fc58879ef860a2dbe80469ef8944fa5ccf
uta-jel3351 <jel3351@mavs.uta.edu>
2025-04-15 15:34:40 -0500

    a couple methods for gpc based on gameManagersubsys

--- checkers/src/main/java/uta/cse3310/GameManager/GamePageController.java
+++ checkers/src/main/java/uta/cse3310/GameManager/GamePageController.java
@@ -1,26 +1,37 @@
 package uta.cse3310.GameManager;
 
-import java.util.ArrayList;
-
 public class GamePageController {
     private GameManager gameManager;
 
     GamePageController(GameManager gameManager){
         this.gameManager = gameManager;
     }
-    public Board sendBoard(){
-        
-        return null;
+    public Board sendBoard(int playerID){
+        Game game = gameManager.findGameByPlayerId(playerID);
+        if(game == null){return null;}
+        return game.getBoard();
     }
 
-    public Game getID(int playerID){
-        
-        return null;
+    public Game returnGame(int playerID){
+        return gameManager.findGameByPlayerId(playerID);
     }
 
-    public Moves receiveMoves(){
+    public Moves receiveMoves(Move move, int playerID){
         Moves moves = new Moves();
-
+        moves.addNext(move);
+        gameManager.processMove(playerID, move);
         return moves; 
+    }
+
+    public int playerTurn(int playerID){
+        Game game = gameManager.findGameByPlayerId(playerID);
+        int turn = game.getCurrentTurn().getPlayerId();
+        return turn;
+    }
+    public Integer isThereWinner(int playerID){
+        Game game = gameManager.findGameByPlayerId(playerID);
+        if(game == null){return null;}
+        if(game.getWinner() == null){return null;}
+        return game.getWinner().getPlayerId();
     }
 }
