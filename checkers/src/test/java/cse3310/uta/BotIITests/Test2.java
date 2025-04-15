b03fe64408d7b7fcc4dbde28152f6abb6507cfd5
JoelBeau <jlb0471@mavs.uta.edu>
2025-04-15 11:08:14 -0500

    Added palce holders for BotII tests

---
+++ checkers/src/test/java/cse3310/uta/BotIITests/Test2.java
@@ -0,0 +1,5 @@
+package cse3310.uta.BotIITests;
+
+public class Test2 {
+    // Unit test 2 for BotII
+}
---
+++ checkers/src/test/java/cse3310/uta/BotIITests/Test3.java
@@ -0,0 +1,5 @@
+package cse3310.uta.BotIITests;
+
+public class Test3 {
+    // Unit test 3 for Bot II
+}
