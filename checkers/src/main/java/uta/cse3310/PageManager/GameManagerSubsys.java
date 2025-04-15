0f48759f3c618697169b7480860020e3c2754f45
priscillal1 <pxl3052@mavs.uta.edu>
2025-04-14 20:58:45 -0500

    simulate game status from Game Manager

--- checkers/src/main/java/uta/cse3310/PageManager/GameManagerSubsys.java
+++ checkers/src/main/java/uta/cse3310/PageManager/GameManagerSubsys.java
@@ -3,21 +3,44 @@ import uta.cse3310.GameManager.GamePageController;
 
 public class GameManagerSubsys {
 
-    GamePageController gamePageController;
+    private GamePageController gamePageController;
+    int playerID;
 
-    public GameManagerSubsys(GamePageController gameStatus) {
-        this.gamePageController = gameStatus;
-     }
-    // will be updating the methods 
-    //public game_status gameInfoFromClientID(int ) {
-        //game_status status = new game_status();
-    
-    //try {
-        //Game game = gamePageController.SendGameFromClientID(ClienID);
-    
-    
-    
-    
+    //public GameManagerSubsys(GamePageController controller) {
+        //this.gamePageController = controller;
     //}
+        //will be updating the methods once GamepageController updates
+    //public void setPlayerID(int id) {
+        //this.playerID = id;
+        //gamePageController.setPlayerID(id); sends player id to gameManager
    // }
+   // public game_status getGameInfo() {
+   //    game_status status = new game_status();
+   //}
+
+    /**
+     * Hardcoded version — simulates what we'd get from GamePageController( when it is implemented) and GameManager.
+     */
+    public GameManagerSubsys()  {
+
+    }
+
+    public game_status getGameInfo() {
+        game_status status = new game_status();
+
+    // fake values 
+      try {
+        status.turn = 1;
+        status.score = 10.0f;
+        status.gameOver = false;
+        status.gameID = 456;
+        status.winner = null;
+        status.loser = null;
+        status.msg = "This is the hardcoded verison";
+        } catch (Exception e) {
+        status.msg = "Error (hardcoded): " + e.getMessage();
+        status.gameOver = true;
+     }
+        return status; 
+    }
 }
