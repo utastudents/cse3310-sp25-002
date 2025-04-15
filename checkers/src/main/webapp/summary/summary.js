f4bb485e3430ae077589991a88fc12696026a5be
Sidharth <125322559+sidc43@users.noreply.github.com>
2025-04-15 18:09:32 -0500

    moved js

---
+++ checkers/src/main/webapp/summary/summary.js
@@ -0,0 +1,10 @@
+// function callPlayerStats() {
+//     call java function and return current player stats
+// return playerStats
+// }
+
+// function showPlayerStats() {
+// const result = callPlayerStats(); 
+//   could implement over several js functions and format in result variable or format callPlayerStats return value 
+// document.getElementById("playerStats").innerText = result;
+// }
