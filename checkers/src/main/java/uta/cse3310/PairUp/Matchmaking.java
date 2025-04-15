b95f11d8dbfdbf95818f6c98eb2e30ff1cdba5ce
ctran42 <cxt4513@mavs.uta.edu>
2025-04-15 14:03:19 -0500

    Added proper creation of bot game

--- checkers/src/main/java/uta/cse3310/PairUp/Matchmaking.java
+++ checkers/src/main/java/uta/cse3310/PairUp/Matchmaking.java
@@ -24,17 +24,28 @@ public class Matchmaking {
         gameManagerCommunication = new GamePairController();
     }
 
-    public void pair(PlayerInMatchmaking p1, PlayerInMatchmaking p2, boolean isBotGame) {
+    // Pairs two players
+    public void pair(PlayerInMatchmaking p1, PlayerInMatchmaking p2) {
         Random coinflip = new Random();
         boolean p1Color = coinflip.nextBoolean();
         boolean p2Color = !p1Color;
-        Match match = new Match(p1.getPlayerID(), p2.getPlayerID(), p1.getPlayerName(), p2.getPlayerName(), isBotGame, gameId++, p1Color, p2Color);
+        Match match = new Match(p1.getPlayerID(), p2.getPlayerID(), p1.getPlayerName(), p2.getPlayerName(), false, gameId++, p1Color, p2Color);
+        gameManagerCommunication.newMatch(match); // Sends match info to gamePairController object for gameController to do what they want with
+    }
+
+    // Pairs a plyer and bot
+    public void pair(PlayerInMatchmaking p1, int botID) {
+        Random coinflip = new Random();
+        boolean p1Color = coinflip.nextBoolean();
+        boolean botColor = !p1Color;
+        Match match = new Match(p1.getPlayerID(), botID, p1.getPlayerName(), "Bot", true, gameId++, p1Color, botColor);
         gameManagerCommunication.newMatch(match); // Sends match info to gamePairController object for gameController to do what they want with
     }
 
     public void addPlayer(int PlayerID, PlayerInMatchmaking newPlayer) {
         if (newPlayer.isPlayAgainstBot()) {
-            // TO-DO: pair bot with user
+            int botID = (int) (Math.random() * 2);
+            pair(newPlayer, botID);
         }
         else {
             players.put(PlayerID, newPlayer);
@@ -56,7 +67,7 @@ public class Matchmaking {
                 // TO-DO: implement matchmaking algorithm
 
                 // Put this line into when a match is made between two humans
-                pair(p1, p2, false);
+                pair(p1, p2);
             }
         }
     }
