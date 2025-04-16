942db06ac59862498dd962224647302e90a58a7c
Akhil Cherukuri <axc5473@mavs.uta.edu>
2025-04-16 10:51:51 -0500

    PairUp test1

--- checkers/src/test/java/cse3310/uta/PairUpTests/Test1.java
+++ checkers/src/test/java/cse3310/uta/PairUpTests/Test1.java
@@ -3,3 +3,12 @@ package cse3310.uta.PairUpTests;
 import org.junit.Test;
 import uta.cse3310.PairUp.PairUp;
 import static org.junit.Assert.*;
+
+public class Test1 
+{
+    @Test
+    public void TestPair()
+    {
+        
+    }
+}
