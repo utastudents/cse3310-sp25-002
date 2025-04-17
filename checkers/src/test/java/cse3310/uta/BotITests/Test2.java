633cbde65272f46ac5bb6802f922ef48988923fe
Taher Husain <txh7180@mavs.uta.edu>
2025-04-16 22:19:54 -0500

    Adding imports to BotI test2

--- checkers/src/test/java/cse3310/uta/BotITests/Test2.java
+++ checkers/src/test/java/cse3310/uta/BotITests/Test2.java
@@ -1,7 +1,14 @@
 package cse3310.uta.BotITests;
 
 import uta.cse3310.Bot.BotI.BotI;
+import uta.cse3310.Bot.Bot;
+import uta.cse3310.GameManager.Board;
+import uta.cse3310.GameManager.Moves;
+import uta.cse3310.GameManager.Move;
+import uta.cse3310.GameManager.Square;
+import java.util.LinkedList;
+import static org.junit.Assert.*;
 
-public class Test2{
-    
+public class Test2 {
+
 }
