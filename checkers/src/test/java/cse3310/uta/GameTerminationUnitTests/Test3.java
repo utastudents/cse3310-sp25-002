66a4331a1948fb53ca72d583325113a1c1fb54aa
uta-sxa8844 <sxa8844@mavs.uta.edu>
2025-04-17 16:38:32 -0500

    Update Test3.java

--- checkers/src/test/java/cse3310/uta/GameTerminationUnitTests/Test3.java
+++ checkers/src/test/java/cse3310/uta/GameTerminationUnitTests/Test3.java
@@ -1,1 +1,23 @@
-//slot for unit test 3
+package cse3310.uta.GameTerminationUnitTests;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.util.Map;
+
+import uta.cse3310.GameTermination.GameTermination;
+
+public class GameTerminationTest {
+
+    @Test
+    public void testGetPlayerHasLegalMoves() {
+        GameTermination termination = new GameTermination();
+        Map<Integer, Boolean> legalMoves = termination.getPlayerHasLegalMoves();
+
+        assertEquals(2, legalMoves.size());
+        assertTrue(legalMoves.containsKey(1));
+        assertTrue(legalMoves.containsKey(2));
+        assertFalse(legalMoves.get(1));  // Player 1 has no legal moves
+        assertTrue(legalMoves.get(2));   // Player 2 has legal moves
+    }
+}
