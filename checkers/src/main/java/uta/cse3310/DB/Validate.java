2d781768205e4b38bf28a0b3d78502305c45ba39
Prabin Lamichhane <pxl0919@mavs.uta.edu>
2025-04-17 16:58:40 -0500

    Validate user updates

--- checkers/src/main/java/uta/cse3310/DB/Validate.java
+++ checkers/src/main/java/uta/cse3310/DB/Validate.java
@@ -5,47 +5,73 @@ import java.sql.SQLException;
 
 public class Validate
 {
-    
-    public static void ValidateUser(String username)
-    {   
-        String [] databaseUsername = new String[10];
-        // DB db = new Db();
-        // String databaseUsername = db.getLeaderboard();
-        for (String users : databaseUsername)
-        {
-            if (users == username || users == null)
+    public static void ValidateUser(String username){
+        try{
+            String sqlString = "SELECT username FROM users WHERE username = ?";
+            try (java.sql.Connection connection = DriverManager.getConnection("jdbc:sqlite:users.db");
+                java.sql.PreparedStatement sqlStatement = connection.prepareStatement(sqlString))
             {
-                System.out.println("401 Invalid username");
-                return;
+                sqlStatement.setString(1, username);
+                java.sql.ResultSet resultSet = sqlStatement.executeQuery();
+                if(resultSet.next()){
+                    System.out.println("401 Invalid username");
+                }else{
+                    System.out.println("201 Username Created");
+                    DB.insertUser(username);
+                    
+                }
             }
-            else
+            catch (SQLException e)
             {
-
-                System.out.println("201 Username Created");
-                String insertStatement = "INSERT INTO users (username) VALUES(?)";
-                // String insert = insertUser(String username);
-                try (java.sql.Connection connection = DriverManager.getConnection("jdbc:sqlite:users.db");
-                    java.sql.PreparedStatement pstmt = connection.prepareStatement(insertStatement))
-                {
-                    if (connection != null)
-                    {
-                        pstmt.setString(1, username);
-                        pstmt.executeUpdate();
-                    }
-                    else
-                    {
-                        System.err.println("Failed to connect to database in Insert User command");
-                    }
-                }
-                catch (SQLException e)
-                {
-                    System.err.println("Error inserting user: " + e.getMessage());
-                }
-                System.out.println("201 Username Created");
-                return;
+                System.err.println("Error in the username insertion: " + e.getMessage());
             }
+            System.out.println("201 Username Created");
+        } catch (Exception e) {
+            System.err.println("Error in the database: " + e.getMessage());
         }
+
     }
-
-
 }
+//     public static void ValidateUser(String username)
+//     {   
+//         String [] databaseUsername = new String[10];
+//         // DB db = new Db();
+//         // String databaseUsername = db.getLeaderboard();
+//         for (String users : databaseUsername)
+//         {
+//             if (users == username || users == null)
+//             {
+//                 System.out.println("401 Invalid username");
+//                 return;
+//             }
+//             else
+//             {
+
+//                 System.out.println("201 Username Created");
+//                 String insertStatement = "INSERT INTO users (username) VALUES(?)";
+//                 // String insert = insertUser(String username);
+//                 try (java.sql.Connection connection = DriverManager.getConnection("jdbc:sqlite:users.db");
+//                     java.sql.PreparedStatement pstmt = connection.prepareStatement(insertStatement))
+//                 {
+//                     if (connection != null)
+//                     {
+//                         pstmt.setString(1, username);
+//                         pstmt.executeUpdate();
+//                     }
+//                     else
+//                     {
+//                         System.err.println("Failed to connect to database in Insert User command");
+//                     }
+//                 }
+//                 catch (SQLException e)
+//                 {
+//                     System.err.println("Error inserting user: " + e.getMessage());
+//                 }
+//                 System.out.println("201 Username Created");
+//                 return;
+//             }
+//         }
+//     }
+
+
+// }
