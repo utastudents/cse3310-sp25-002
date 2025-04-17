260260c4f298581110290eee489eb3535e817960
Taher Husain <txh7180@mavs.uta.edu>
2025-04-17 15:59:47 -0500

    Adding placeholder for test1 BotI

--- checkers/src/test/java/cse3310/uta/BotITests/Test1.java
+++ checkers/src/test/java/cse3310/uta/BotITests/Test1.java
@@ -1,7 +1,19 @@
 package cse3310.uta.BotITests;
 
 import uta.cse3310.Bot.BotI.BotI;
+import uta.cse3310.Bot.Bot;
+import uta.cse3310.GameManager.Board;
+import uta.cse3310.GameManager.Moves;
+import uta.cse3310.GameManager.Move;
+import uta.cse3310.GameManager.Square;
+import java.util.LinkedList;
+import static org.junit.Assert.*;
+import org.junit.Test;
 
-public class Test1{
-    
+public class Test1 {
+    @Test
+    public void testingPlayCapture() {
+        /* Placeholder for testing playCature */
+    }
+
 }
