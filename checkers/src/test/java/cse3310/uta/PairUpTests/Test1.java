e97324c4776fe1b2b456e83bfa4489614cb74c32
utaixa5131 <ixa5131@mavs.uta.edu>
2025-04-17 03:24:45 -0500

    Create Search Function in Pairup and Implement Tests

--- checkers/src/test/java/cse3310/uta/PairUpTests/Test1.java
+++ checkers/src/test/java/cse3310/uta/PairUpTests/Test1.java
@@ -2,30 +2,20 @@ package cse3310.uta.PairUpTests;
 
 import org.junit.Test;
 import uta.cse3310.PairUp.*;
-import uta.cse3310.GameManager.GamePairController;
-import uta.cse3310.GameManager.Game;
 import static org.junit.Assert.*;
 
 public class Test1 
 {
     @Test
-    public void TestPairWithPlayers()
+    public void TestAddPlayer()
     {
-        PlayerInMatchmaking p1 = new PlayerInMatchmaking(171328890285L,451, "Alice",false,9);
-        PlayerInMatchmaking p2 = new PlayerInMatchmaking(171328890789L,452, "Bob",false,10);
+        PairUp pairUp = new PairUp();
 
-        Matchmaking matchmaking = new Matchmaking();
+        pairUp.AddPlayer(171328890285L,451, "Alice",false,9);
+    
+        assertEquals(true, pairUp.searchPlayer(451));
+        
+        
 
-        // Act
-        Game game = matchmaking.pair(p1, p2);  // now it returns the Game object
-
-        // Assert
-        assertEquals(451, game.getPlayer1ID());
-        assertEquals(452, game.getPlayer2ID());
-
-        // Check that exactly one of the players has "true" color
-        boolean p1Color = game.getPlayer1Color();
-        boolean p2Color = game.getPlayer2Color();
-        assertNotEquals(p1Color, p2Color); 
     }
 }
