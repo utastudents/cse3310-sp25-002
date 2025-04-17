be90c1e359b668bbc83c691e6249668da785b7fe
Burhanuddin Aliasgar Khokhar <bxk9892@mavs.uta.edu>
2025-04-16 23:47:30 -0500

    adding test case for removePlayer

--- checkers/src/test/java/cse3310/uta/PairUpTests/Test2.java
+++ checkers/src/test/java/cse3310/uta/PairUpTests/Test2.java
@@ -1,19 +1,35 @@
 package cse3310.uta.PairUpTests;
 
 import org.junit.Test;
+
+import uta.cse3310.Bot.BotII.Pair;
+import uta.cse3310.PairUp.Match;
+import uta.cse3310.PairUp.Matchmaking;
+import uta.cse3310.PairUp.PlayerInMatchmaking;
 import uta.cse3310.PairUp.PairUp;
 import static org.junit.Assert.*;
 
 public class Test2 
 {
-    /*initialize matchmaking object and playerInMatchmaking object
+    /*initialize a PairUp object
     add players to matchmaking
-    check if the player is in the matchmaking using assertTrue
+    check if the player is in the matchmaking using assertEquals
     test for removing players:
-    check if the player is removed from matchmaking using assertFalse */
+    check if the player is removed from matchmaking using assertEquals */
+    
+    
+
     @Test
     public void TestRemovePlayer()
     {
-        
+        PairUp pairUp = new PairUp();
+
+        pairUp.AddPlayer(151124840883L, 108, "Michael", false, 2);
+
+        assertEquals(true, pairUp.searchPlayer(108));
+
+        pairUp.removePlayer(108);
+
+        assertEquals(false, pairUp.searchPlayer(108));
     }
 }
