c94b9bdcbd017d2b48bb67c608a3201641906878
ctran42 <cxt4513@mavs.uta.edu>
2025-04-16 23:04:08 -0500

    Fixed test cases

--- checkers/src/test/java/cse3310/uta/PairUpTests/Test3.java
+++ checkers/src/test/java/cse3310/uta/PairUpTests/Test3.java
@@ -1,7 +1,7 @@
 package cse3310.uta.PairUpTests;
 
 import org.junit.Test;
-import uta.cse3310.PairUp.Matchmaking;
+import uta.cse3310.PairUp.*;
 import static org.junit.Assert.*;
 
 public class Test3 
@@ -9,6 +9,13 @@ public class Test3
     @Test
     public void TestAddPlayer()
     {
+        PairUp pairUp = new PairUp();
+
+        pairUp.AddPlayer(171328890285L,451, "Alice",false,9);
+    
+        assertEquals(true, pairUp.searchPlayer(451));
         
+        
+
     }
 }
