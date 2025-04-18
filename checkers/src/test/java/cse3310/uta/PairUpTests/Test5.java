e74f7f0ce63b8bcc509c49b71f3d4c6d03b3052e
Burhanuddin Aliasgar Khokhar <bxk9892@mavs.uta.edu>
2025-04-17 22:50:33 -0500

    removing unused imports from test5

--- checkers/src/test/java/cse3310/uta/PairUpTests/Test5.java
+++ checkers/src/test/java/cse3310/uta/PairUpTests/Test5.java
@@ -1,9 +1,8 @@
 package cse3310.uta.PairUpTests;
 
-import java.util.ArrayList;
+
 import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
