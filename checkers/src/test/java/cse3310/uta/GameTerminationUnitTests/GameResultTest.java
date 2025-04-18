65025c77c16fa1cfe58baa41ee35157492cf523c
mpg4293 <mpg4293@mavs.uta.edu>
2025-04-17 23:08:20 -0500

    updated GameResultTest

--- checkers/src/test/java/cse3310/uta/GameTerminationUnitTests/GameResultTest.java
+++ checkers/src/test/java/cse3310/uta/GameTerminationUnitTests/GameResultTest.java
@@ -7,19 +7,21 @@ import java.util.HashMap;
 import java.util.Map;
 
 import uta.cse3310.GameManager.Player;
+import uta.cse3310.GameTermination.GameResult;
 
-public class GameResultTest {
-    //test for the updateScores() and getScore() method from GameResult
+public class GameResultTest 
+{
     @Test
-    public void testUpdateScoresAndGetScore() {
-        Map<String, Integer> scores = new HashMap<>();
-        //GameResult result = new GameResult(scores);
+    public void testUpdateScoresAndGetScore() 
+    {
+        Map<Integer, Integer> scores = new HashMap<>();
+        GameResult result = new GameResult(scores);
 
-        //Player player = new Player(1); // Assuming player ID = 1
-        // result.updateScores(player, 20);
+        Player player = new Player(1, true);
+        result.updateScores(player, 20);
 
-        // int actualScore = result.getScore("1");
+        int actualScore = result.getScore(1);
 
-        //assertEquals(20, actualScore);
+        assertEquals(20, actualScore);
     }
 }
