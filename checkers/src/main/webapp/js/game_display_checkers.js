87a86a01fcf15dc3a679ce9b1815c9ed7d7d7f95
AllenVu <ahv8830@mavs.uta.edu>
2025-04-27 22:17:17 -0500

    added get piece color function

--- checkers/src/main/webapp/js/game_display_checkers.js
+++ checkers/src/main/webapp/js/game_display_checkers.js
@@ -207,6 +207,13 @@ class CheckersBoard {
         this.last_requested_moves = null;
     }
 
+    //function is used to get the color of the piece
+    get_piece_color(piece_type){
+        if (piece_type === 'w' || piece_type === 'W')
+            return 'W';
+        if (piece_type === 'b' || piece_type === 'B')
+            return 'B';
+    }
 
     update_current_player(player, player_id) {
         // Update the UI to show whose turn it is
