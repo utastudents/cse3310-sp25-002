b6f9e19441fff9b5c8487a9db9a480e5c4f027db
Lekhak123 <hxl7195@mavs.uta.edu>
2025-04-29 10:01:58 -0500

    updated test case

--- checkers/src/test/java/cse3310/uta/GamePlayTests/Test1.java
+++ checkers/src/test/java/cse3310/uta/GamePlayTests/Test1.java
@@ -1,7 +1,6 @@
 package cse3310.uta.GamePlayTests;
 
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
 import org.junit.Test;
 
 import uta.cse3310.GameManager.Board;
@@ -11,26 +10,24 @@ import uta.cse3310.GameManager.Move;
 import uta.cse3310.GamePlay.rules;
 
 
-
-public class Test1 extends rules 
+public class Test1 extends rules
 {
-    
+
 
     @Test
     public void canMovePieceTest()
     {
         Board board = new Board();
         board.initializeBoard();
-        
+
+        Game game = new Game(0, 1, false, true, 2);
+
         Square startSquare = board.getSquare(2, 1);
-        Square destSquare = board.getSquare(3, 2);
-        Game game = new Game(0,1,false,true,2);
+        Square destSquare = board.getSquare(3, 0);
+
         Move testMove = new Move(startSquare, destSquare);
 
-        Square initialBlackPiece = board.getSquare(2,1);
-        Square potentialDestination = board.getSquare(3,0);
-        Move initialMove = new Move(initialBlackPiece, potentialDestination);
-        assertTrue(rules.canMovePiece(game, initialMove));
+        assertTrue("Black should be able to make the opening move 2,1 -> 3,0", rules.canMovePiece(game, testMove));
     }
-    
+
 }
