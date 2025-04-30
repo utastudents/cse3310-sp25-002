2a0f62dcb3145260b589c0ec612cdfde3ad43abe
sushant-sah <sxs1952@mavs.uta.edu>
2025-04-30 01:09:41 -0400

    Requirements_Tests.java
    File created to add unit test to test requirments

--- checkers/src/test/java/cse3310/uta/GameManagerTests/Requirements_Tests.java
+++ checkers/src/test/java/cse3310/uta/GameManagerTests/Requirements_Tests.java
@@ -1,1 +1,17 @@
+package cse3310.uta.GameManagerTests;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
 
+import uta.cse3310.GameManager.GameManager;
+import uta.cse3310.GameManager.Game;
+import uta.cse3310.GameManager.Square;
+import uta.cse3310.GameManager.Board;
+import uta.cse3310.GameManager.GamePageController;
+import uta.cse3310.GameManager.GamePairController;
+import uta.cse3310.GameManager.Move;
+import uta.cse3310.GameManager.Moves;
+import uta.cse3310.GameManager.Player;
+import uta.cse3310.PairUp.Match;
+
+public class Requirements_Tests {}
