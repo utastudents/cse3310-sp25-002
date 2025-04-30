17eb4959dd44b7afe624e71e4aed07baa5fc8561
AleidaRamirez2026 <asr4376@mavs.uta.edu>
2025-04-30 15:20:43 -0500

    minor grammar issues DB.java

--- checkers/src/main/java/uta/cse3310/DB/DB.java
+++ checkers/src/main/java/uta/cse3310/DB/DB.java
@@ -38,18 +38,23 @@ public class DB
 			return -1;
 	}
 	try (Connection connection = SQLiteConnector.connect();
-	     PreparedStatement pstmt = connection.prepareStatement(insertStatement, Statement.RETRURN_GENERATED_KEYS)) {
+	     PreparedStatement pstmt = connection.prepareStatement(insertStatement, Statement.RETURN_GENERATED_KEYS)) {
 
 		pstmt.setString(1, username.trim());
 		pstmt.executeUpdate();
 
-		try(ResulSet generateKeys = pstmt.getGeneratedKeys()) {
+		try(ResultSet generatedKeys = pstmt.getGeneratedKeys()) {
 			if (generatedKeys.next()) {
 				generatedId = generatedKeys.getInt(1);
 			}
 		}
 	} catch (SQLException e) {
-		System.err.printIn("Error inserting user: "+ e.getMessage());
+		if (e.getMessage().toLowerCase().contains("unique") || e.getMessage().toLowerCase().contains("constraint")) {
+		System.err.println("Username unavailable: "+username);
+		return -2; //-2 means username is already taken
+		} else{
+			System.err.println("Error inserting user: "+ e.getMessage());
+		}
 	}
 	return generatedId;
 	}
@@ -77,22 +82,22 @@ public class DB
 		return leaderboard;
 	}
 
-	public static void updatePlayer(String username, int newRank )			
+	public static boolean updatePlayer(String username, int newRank )			
 	{
 		String updatePlayer = "UPDATE USERS SET rank = ? WHERE username = ?";  	
 		boolean success = false;
 
-		if (usename == null || username.trim().isEmpty()) {
+		if (username == null || username.trim().isEmpty()) {
 			System.err.println("Username is null or empty");
 			return false;
 		}
 		try(Connection connection = SQLiteConnector.connect();
-			PreparedStatement pstmt = connection.prepareStatement(updatePlayer)) 
+			PreparedStatement pstmt = connection.prepareStatement("UPDATE USERS SET rank=? WHERE username = ?")) 
 		{
 				pstmt.setInt(1, newRank); 			//sets the newRank into the placeholder
 				pstmt.setString(2, username.trim());	
 				int rowsUpdated = pstmt.executeUpdate();
-				success = rowsUpdateed > 0;
+				success = rowsUpdated > 0;
 			
 		}
 		catch(SQLException e)
@@ -103,3 +108,4 @@ public class DB
 	return success;
 }
 
+}
